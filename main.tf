52259977ab642a93dd77acc1b51e9af2924f3acb
RohanS <rohansomkuwar97@gmail.com>
2022-08-09 12:21:18 +0530

    using terrafom script creagte docker image and contqainer

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Start a container
+resource "docker_container" "nginx" {
+  name  = "nginx"
+  image = docker_image.nginx_image.latest
+
+  ports {
+    internal = "80"
+    external = "8080"
+  }
+}
+resource "docker_image" "nginx_image" {
+  name = "nginx"
+}
